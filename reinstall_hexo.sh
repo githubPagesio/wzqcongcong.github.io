83ca05b65148d8e5a137e6414d9b6d3c59373f8e
wzqcongcong <wzqcongcong@sina.com>
2017-07-26 14:53:08 +0800

    add tool to reinstall hexo

---
+++ reinstall_hexo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+npm uninstall hexo-cli -g
+npm uninstall hexo -g
+npm uninstall hexo-cli
+npm uninstall hexo
+
+npm install hexo -g
+npm install hexo-cli -g
+npm install hexo
+npm install hexo-cli
